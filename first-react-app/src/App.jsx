ea51f1e7e4471dda67c87858d3c752dabbe74cbe
RomanasV <romanasvenckus@gmail.com>
2025-02-06 18:18:49 +0200

    set up for eventsPage

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -1,43 +1,11 @@
-import './App.css'
-import Services from './components/Services/Services'
-// import Goods from './components/Goods/Goods'
-// import Navigation from './components/Navigation/Navigation'
-
-// import logo from './assets/react.svg'
-// import CustomButton from './components/CustomButton'
-// import Container from './components/Container/Container'
+// import BasicsPage from './pages/BasicsPage/BasicsPage'
+import EventsPage from "./pages/EventsPage"
 
 function App() {
   return (
     <>
-      {/* <Container>
-        <Navigation />
-      </Container> */}
-
-      <main>
-        {/* <h1>Mano puslapis</h1> */}
-
-        <Services />
-        {/* <Goods /> */}
-      </main>
-
-      {/* <footer>
-        <Container>
-          <p>Footer</p>
-
-          <CustomButton>Hello</CustomButton>
-
-          <CustomButton test='labas'>
-            <img src={logo} /> Hello
-          </CustomButton>
-
-          <CustomButton>
-            <div>Good</div>
-            <div>Evening</div>
-          </CustomButton>
-        </Container>
-        
-      </footer> */}
+      {/* <BasicsPage /> */}
+      <EventsPage />
     </>
   )
 }
