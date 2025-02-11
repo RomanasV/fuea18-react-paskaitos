50ddb65facc9563e0858113d818f570ffa0dfeae
RomanasV <romanasvenckus@gmail.com>
2025-02-11 18:54:08 +0200

    todo task done

--- first-react-app/src/pages/TodoPage/TodoPage.jsx
+++ first-react-app/src/pages/TodoPage/TodoPage.jsx
@@ -1,11 +1,26 @@
 import { useState } from "react"
+import './TodoPage.css'
+
+const initialTodos = [
+    {
+        title: 'Vienas',
+        description: 'Aprasymas',
+        id: Math.random(),
+        isDone: false
+    },
+    {
+        title: 'Du',
+        description: 'Aprasymas 2',
+        id: Math.random(),
+        isDone: true
+    },
+]
 
 function TodoPage() {
     const [title, setTitle] = useState('')
     const [description, setDescription] = useState('')
 
-    const [todos, setTodos] = useState([])
-    console.log(todos)
+    const [todos, setTodos] = useState(initialTodos)
 
     const titleHandler = event => setTitle(event.target.value)
     const descriptionHandler = event => setDescription(event.target.value)
@@ -17,24 +32,81 @@ function TodoPage() {
             title,
             description,
             id: Math.random(),
+            isDone: false
         }
-
-        console.log(newTodo)
 
         setTodos(prevState => [newTodo, ...prevState])
     }
 
+    const deleteHandler = id => {
+        setTodos(prevState => prevState.filter(todo => todo.id !== id))
+    }
+
+    const doneHandler = id => {
+        setTodos(prevState => {
+            const updatedState = prevState.map(todo => {
+                if (id === todo.id) {
+                    const updatedTodo = { ...todo, isDone: !todo.isDone }
+                    return updatedTodo
+                }
+
+                return todo
+            })
+
+            return updatedState
+        })
+    }
+
     return (
-        <div>
+        <div id="todo-page">
             <form onSubmit={newTaskHandler}>
-                <input value={title} onChange={titleHandler} type="text" />
-                <textarea value={description} onChange={descriptionHandler}></textarea>
-                <input type="date" />
+                <div>
+                    <label htmlFor="task">Task:</label>
+                    <input value={title} onChange={titleHandler} type="text" id="task" />
+                </div>
+
+                <div>
+                    <label htmlFor="description">Description</label>
+                    <textarea value={description} onChange={descriptionHandler} id="description"></textarea>
+                </div>
+
+                {/* <div>
+                    <label htmlFor="due-date">Due Date:</label>
+                    <input type="date" id="due-date" />
+                </div> */}
 
                 <button type="submit">Create New Task</button> 
             </form>
 
-            {todos.map(item => <p key={item.id}>{item.title}</p>)}
+
+            <div className="tasks-container">
+                <h2>Tasks:</h2>
+
+                <div className="tasks-list">
+
+                    {todos.map(todo => {
+                        console.log(todo.id)
+                        console.log(todo.isDone)
+
+                        return (
+                            <div key={todo.id} className={`task-item ${todo.isDone ? ' done' : ''}`}>
+                                <span>{todo.id}</span>
+                                <h3 className="task-title">{todo.title}</h3>
+                                <p className="task-description">{todo.description}</p>
+                                <p>Task status: {todo.isDone ? 'Done' : 'Not Done'}</p>
+
+                                <button onClick={() => doneHandler(todo.id)}>{todo.isDone ? 'Undo' : 'Done'}</button>
+                                <button onClick={() => deleteHandler(todo.id)}>Delete</button>
+                            </div>
+                        )
+                    })}
+
+
+                </div>
+            </div>
+
+
+
         </div>
     )
 }
